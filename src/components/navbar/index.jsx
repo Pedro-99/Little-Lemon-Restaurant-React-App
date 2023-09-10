cf27b98f1eb8cdf2bdcfa9838304705a98d9bdca
zineddaine <zidan99badr@gmail.com>
2023-09-10 22:45:49 +0100

    navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>
-        <ul>
-            <li><Link to={'/home'}>Home</Link></li>
-            <li><Link to={'/menu'}>Menu</Link></li>
-            <li><Link to={'/about'}>About</Link></li>
+    <nav className='d-flex align-items-center justify-content-center'>
+        <ul className='d-flex justify-content-around list-unstyled'>
+            <li className='mt-2 px-5'><Link className='text-decoration-none' to={'/home'}>Home</Link></li>
+            <li className='mt-2 px-5'><Link className='text-decoration-none' to={'/about'}>About</Link></li>
+            <li className='mt-2 px-5'><Link className='text-decoration-none' to={'/menu'}>Menu</Link></li>
+            <li className='mt-2 px-5'><Link className='text-decoration-none' to={'/booking'}>Booking</Link></li>
         </ul>
     </nav>
   )
