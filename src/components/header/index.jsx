1b33f08965998ce815c85866881a7fffd0ed416a
zineddaine <zidan99badr@gmail.com>
2023-09-10 22:44:22 +0100

    header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.css';
 
 export default function Header(props) {
     const { children } = props;
   return (
-    <header>
+    <header className='bg-header border shadow-sm p-5 d-flex justify-content-center align-items-center'>
         {children}
     </header>
   )
