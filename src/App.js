0bf00e7509f372bec7973a6d595e3f369b2db98b
zineddaine <zidan99badr@gmail.com>
2023-09-10 21:35:21 +0100

    add booking page route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import BookingPage from './pages/booking';
 import LandingPage from './pages/landing-page';
 import { Route, Routes } from "react-router-dom"
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/booking" element={<BookingPage />} />
     </Routes>
   );
 }
