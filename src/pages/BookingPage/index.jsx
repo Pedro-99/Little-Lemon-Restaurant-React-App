d8465f00563cfddbf6f87db18c50d50fd100e173
zineddaine <zidan99badr@gmail.com>
2023-09-13 20:13:42 +0100

    pass down the state to child component

--- src/pages/BookingPage/index.jsx
+++ src/pages/BookingPage/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import BookingForm from '../../components/BookingForm';
 import Navbar from '../../components/navbar';
 import Header from '../../components/header';
 
 export default function BookingPage() {
+    const [bookingForm, setBookingForm] = useState({
+        date: `${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`,
+        time: '17:00',
+        numberOfGuests: 1,
+        occasion: 'birthday',
+    });
+
+    const updateTimes = (date) => {
+        // In the Future, remember to add a switch statement - you need to display the availble times based on each date
+        // for now we will let the availble times the same no matter the date is
+        return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+      };
+
+
+    const initializeTimes = () => {
+        return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+      };
+
+      const [availableTimes, dispatchTimes] = useReducer(
+        (state, action) => {
+          switch (action.type) {
+            case 'UPDATE_TIMES':
+              return updateTimes(action.payload.date);
+            default:
+              return state;
+          }
+        },
+        initializeTimes()
+      );
+
+
+    const handleState = (e) => {
+        e.preventDefault();
+        setBookingForm((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+
+        dispatchTimes({ type: 'UPDATE_TIMES', payload: { date: e.target.value } });
+    };
+
     return (
         <>
             <Header>
@@ -11,7 +52,7 @@ export default function BookingPage() {
             </Header>
             <div className='container'>
                 <div className="row d-flex justify-content-center p-5">
-                    <BookingForm />
+                    <BookingForm availableTimes={availableTimes} dispatchTimes={dispatchTimes} booking={bookingForm} handleChange={handleState} />
                 </div>
             </div>
         </>
