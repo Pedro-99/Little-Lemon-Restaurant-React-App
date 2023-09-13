4c42f5f70f0f57da73744969b54f29926a5eb701
zineddaine <zidan99badr@gmail.com>
2023-09-13 20:12:47 +0100

    lifting the state up

--- src/components/BookingForm/index.jsx
+++ src/components/BookingForm/index.jsx
@@ -1,44 +1,7 @@
-import React, { useState, useEffect } from 'react';
 
-export default function BookingForm() {
-    const [bookingForm, setBookingForm] = useState({
-        date: `${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`,
-        time: '17:00',
-        numberOfGuests: 1,
-        occasion: 'birthday',
-    });
 
-    const [availableTimes] = useState([
-        '07:00',
-        '08:00',
-        '09:00',
-        '10:00',
-        '11:00',
-        '12:00',
-        '13:00',
-        '14:00',
-        '15:00',
-        '16:00',
-        '17:00',
-        '18:00',
-        '19:00',
-        '20:00',
-        '21:00',
-        '22:00',
-    ]);
-
-    const handleState = (e) => {
-        e.preventDefault();
-        setBookingForm((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    useEffect(() => {
-        console.log(bookingForm);
-    }, [bookingForm]);
-
+export default function BookingForm(props) {
+    const { booking, handleChange, availableTimes } = props;
     return (
         <form
             style={{ display: 'grid', maxWidth: '400px', gap: '20px', width: '400px' }}
@@ -48,15 +11,15 @@ export default function BookingForm() {
                 type="date"
                 id="res-date"
                 name="date"
-                value={bookingForm?.date}
-                onChange={handleState}
+                value={booking?.date}
+                onChange={handleChange}
             />
             <label htmlFor="res-time">Choose time</label>
             <select
                 id="res-time"
                 name="time"
-                value={bookingForm?.time}
-                onChange={handleState}
+                value={booking?.time}
+                onChange={handleChange}
             >
                 {availableTimes?.map((time) => (
                     <option key={time} value={time}>
@@ -72,15 +35,15 @@ export default function BookingForm() {
                 max="10"
                 id="guests"
                 name="numberOfGuests"
-                value={bookingForm?.numberOfGuests}
-                onChange={handleState}
+                value={booking?.numberOfGuests}
+                onChange={handleChange}
             />
             <label htmlFor="occasion">Occasion</label>
             <select
                 id="occasion"
                 name="occasion"
-                value={bookingForm?.occasion}
-                onChange={handleState}
+                value={booking?.occasion}
+                onChange={handleChange}
             >
                 <option>Birthday</option>
                 <option>Anniversary</option>
